80d0dbdf8f57df3d4e187253d54e78e6371ca659
John Days <johnydays0@gmail.com>
2015-09-07 15:54:39 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	path := os.Args[1]
+	fmt.Println(path)
+
+	jsonString := os.Args[2]
+
+	var tree map[string]interface{}
+	err1 := json.Unmarshal([]byte(jsonString), &tree)
+
+	values := make(map[string]interface{})
+
+	for key, value := range tree {
+		values[key] = deepGet(value, path)
+	}
+
+	outputJSON, err := json.Marshal(values)
+
+	fmt.Println(err1)
+	fmt.Println(err)
+	fmt.Println(string(outputJSON))
+}
+
+// function taken from @jwilder/docker-gen
+func deepGet(item interface{}, path string) interface{} {
+	if path == "" {
+		return item
+	}
+
+	path = strings.TrimPrefix(path, ".")
+	parts := strings.Split(path, ".")
+	itemValue := reflect.ValueOf(item)
+
+	if len(parts) > 0 {
+		switch itemValue.Kind() {
+		case reflect.Struct:
+			fieldValue := itemValue.FieldByName(parts[0])
+			if fieldValue.IsValid() {
+				return deepGet(fieldValue.Interface(), strings.Join(parts[1:], "."))
+			}
+		case reflect.Map:
+			mapValue := itemValue.MapIndex(reflect.ValueOf(parts[0]))
+			if mapValue.IsValid() {
+				return deepGet(mapValue.Interface(), strings.Join(parts[1:], "."))
+			}
+		default:
+			return nil
+		}
+
+	}
+
+	return itemValue.Interface()
+}
